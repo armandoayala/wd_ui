6d4bea71dcf9307882ab1c8b376a4946baac9730
Jorge Armando Ayala <armandolnayala@gmail.com>
2020-08-23 20:31:09 -0300

    Cambio link a backend

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,12 +1,12 @@
 //Variable Global para usar en la App
 //Local: http://localhost:3788/api/workdesk/
 //Remote: https://stark-taiga-97026.herokuapp.com/api/workdesk/
-/*export var GLOBAL= {
+export var GLOBAL= {
   url: 'http://localhost:3788/api/workdesk/',
   keyIdentity:"wdIdentity"
-}*/
-export var GLOBAL= {
+}
+/*export var GLOBAL= {
   url: 'https://stark-taiga-97026.herokuapp.com/api/workdesk/',
   keyIdentity:"wdIdentity"
-}
+}*/
 
